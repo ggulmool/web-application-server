6b72105cdd5595f61f64be23c995d1a26db0d748
GGuLMooL <kimny86@gmail.com>
2018-05-06 15:24:25 +0900

    feat : 요구사항1 - index.html응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,9 +22,22 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.info("request line : {}", line);
+
+            if (line == null) {
+                return;
+            }
+
+            String[] tokens = line.split(" ");
+            while (!"".equals(line)) {
+                line = br.readLine();
+                //log.info("header : {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(Paths.get("./webapp" + tokens[1]));
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
