020ca012757c85fbd9a1a3513473ef8297017728
ooa1769 <kimny86@gmail.com>
2018-05-12 23:48:12 +0900

    refactor(3단계) : 추출한 메소드 Controller 인터페이스 추상화

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import controller.Controller;
 import db.DataBase;
 import http.HttpRequest;
 import http.HttpResponse;
@@ -30,79 +31,20 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
-            String path = getDefaultPath(request.getPath());
 
-            if ("/user/create".equals(path)) {
-                createUser(request, response);
-            } else if ("/user/login".equals(path)) {
-                login(request, response);
-            } else if ("/user/list".equals(path)) {
-                listUser(request, response);
-            } else {
+            Controller controller = RequestMapping.getController(request.getPath());
+
+            if (controller == null) {
+                String path = getDefaultPath(request.getPath());
                 response.forward(path);
+            } else {
+                controller.service(request, response);
             }
-
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    private void listUser(HttpRequest request, HttpResponse response) {
-        if (!isLogin(request.getHeader("Cookie"))) {
-            response.sendRedirect("/user/login.html");
-            return;
-        }
-
-        Collection<User> users = DataBase.findAll();
-        StringBuilder sb = new StringBuilder();
-        sb.append("<table border='1'>");
-        for (User user : users) {
-            sb.append("<tr>");
-            sb.append("<td>" + user.getUserId() + "</td>");
-            sb.append("<td>" + user.getName() + "</td>");
-            sb.append("<td>" + user.getEmail() + "</td>");
-            sb.append("<tr>");
-        }
-        sb.append("</table>");
-        response.forwardBody(sb.toString());
-    }
-
-    private void login(HttpRequest request, HttpResponse response) {
-        User user = DataBase.findUserById(request.getParameter("userId"));
-
-        if (user != null) {
-            if (user.getPassword().equals(request.getParameter("password"))) {
-                response.addHeader("Set-Cookie", "logined=true");
-                response.sendRedirect("/index.html");
-            } else {
-                response.sendRedirect("/user/login_failed.html");
-            }
-        } else {
-            response.sendRedirect("/user/login_failed.html");
-        }
-    }
-
-    private void createUser(HttpRequest request, HttpResponse response) {
-        User user = new User(
-                request.getParameter("userId"),
-                request.getParameter("password"),
-                request.getParameter("name"),
-                request.getParameter("email"));
-        log.info("user : {}", user);
-        DataBase.addUser(user);
-        response.sendRedirect("/index.html");
-    }
-
-    private boolean isLogin(String cookieValue) {
-        Map<String, String> cookies = HttpRequestUtils.parseCookies(cookieValue);
-        String value = cookies.get("logined");
-        if (value == null) {
-            return false;
-        }
-        return Boolean.parseBoolean(value);
     }
 
     private String getDefaultPath(String path) {
